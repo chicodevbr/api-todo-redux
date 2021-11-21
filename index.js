025fa28e65b799b2e5deea31da9665d6b626ce9f
chicodevbr <chicodevbr@gmail.com>
2021-11-21 10:13:40 -0300

    change name port

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.get('/', (req, res) => {
 
 const connection_string = process.env.CONNECTION_STRING;
 
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running on port ${port}...`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...`);
 });
 
 mongoose
